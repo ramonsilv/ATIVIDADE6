7a46108434fc3b73c3159269bf863c87342f5db3
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-06-15 15:25:34 -0300

    final de da atividade6 feito em sala

--- js/script.js
+++ js/script.js
@@ -13,7 +13,27 @@ function tabuada(){
 
 // EXERCICIO 2
 
-function numeroPares(){
+function numerosPares() {
     numero = document.getElementById('numer').value
-    
+    resultado = ""
+
+    for (i = 2; i <= numero; i += 2) {
+        resultado += i + ", "
+    }
+
+    document.getElementById('result').innerHTML = "São numeros Pares: " + resultado
+}
+
+// EXERCICIO 3
+
+function soma() {
+    numero = parseInt(document.getElementById('num3').value)
+    soma = 0
+
+    while (numero !== -1) {
+        soma += numero
+        numero = parseInt(prompt("Digite um número (digite -1 para parar):"))
+    }
+
+    document.getElementById('resultado3').innerHTML = "A soma dos números é: " + soma
 }
